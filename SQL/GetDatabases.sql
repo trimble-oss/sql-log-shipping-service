ebe85b6693b25792fa2b344149723546c9b832a4
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2023-07-05 15:19:33 +0100

    Exclude databases already restoring
    Exclude databases with a RESTORE DATABASE operation in progress.  Databases were already excluded if initialized using this tool.

--- SQL/GetDatabases.sql
+++ SQL/GetDatabases.sql
@@ -15,4 +15,13 @@ FROM sys.databases d
 LEFT OUTER JOIN LR ON d.name =  LR.destination_database_name AND LR.rnum=1
 WHERE (d.state = 1 OR d.is_in_standby=1)
 AND d.recovery_model <> 3 /* Exclude Simple */
+/* Exclude databases with a restore in progress */
+AND NOT EXISTS(	SELECT 1 
+				FROM sys.dm_exec_requests R
+				JOIN sys.dm_tran_locks L ON R.session_id = l.request_session_id
+				WHERE R.command='RESTORE DATABASE'
+				AND L.request_mode='X'
+				AND L.resource_type='DATABASE'
+				AND L.resource_database_id = d.database_id
+				)
 ORDER BY LR.backup_finish_date
