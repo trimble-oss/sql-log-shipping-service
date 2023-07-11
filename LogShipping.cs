35fe71a7998333432e3f6cb5c328b283e62b1666
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2023-07-11 10:33:26 +0100

    Fix kill connections
    We need some whitespace between {Config.KillUserConnectionsWithRollBackAfter} and END

--- LogShipping.cs
+++ LogShipping.cs
@@ -319,9 +319,9 @@ namespace LogShippingService
             if (Config.KillUserConnections)
             {
                 var sql = $"IF DATABASEPROPERTYEX({db.SqlSingleQuote()},'IsInStandBy')=1\n";
-                sql += "BEGIN";
-                sql += $"\tALTER DATABASE {db.SqlQuote()} SET SINGLE_USER WITH ROLLBACK AFTER {Config.KillUserConnectionsWithRollBackAfter}";
-                sql += "END";
+                sql += "BEGIN\n";
+                sql += $"\tALTER DATABASE {db.SqlQuote()} SET SINGLE_USER WITH ROLLBACK AFTER {Config.KillUserConnectionsWithRollBackAfter}\n";
+                sql += "END\n";
                 Log.Warning("User connections to {db} are preventing restore operations.  Sessions will be killed after {seconds}. {sql}", db, Config.KillUserConnectionsWithRollBackAfter, sql);
                 try
                 {
