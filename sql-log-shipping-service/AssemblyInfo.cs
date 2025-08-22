1bd2fb1ea807a23f0e2b58d753a93ea7d764e33f
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2025-08-22 14:56:09 +0100

    Bump version

--- sql-log-shipping-service/AssemblyInfo.cs
+++ sql-log-shipping-service/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2022 Trimble, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.7.3")]
+[assembly: AssemblyVersion("1.7.4")]
