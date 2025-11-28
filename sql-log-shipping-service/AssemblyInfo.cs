9193dde5922f161be07fb06968a6f133ac4bea18
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2025-11-28 14:25:58 +0000

    Bump version
    Major version update for change from .NET 8 to .NET 10

--- sql-log-shipping-service/AssemblyInfo.cs
+++ sql-log-shipping-service/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2022 Trimble, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.7.4")]
+[assembly: AssemblyVersion("2.0.0")]
