c014641f929e5401926914e017c6c535260f8528
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2026-03-31 14:30:38 +0100

    GetDatabases query performance fix
    Fix issue with bad query plan - restorehistory/backupset table scan executed multiple times due to nested loop join.  Observed on 1 instance.

--- sql-log-shipping-service/SQL/GetDatabases.sql
+++ sql-log-shipping-service/SQL/GetDatabases.sql
@@ -1,4 +1,9 @@
-﻿WITH LR AS (
+﻿CREATE TABLE #LastRestore(
+			destination_database_name SYSNAME NOT NULL PRIMARY KEY,
+			backup_finish_date DATETIME NULL, 
+			restore_date DATETIME NULL
+);
+WITH LR AS (
 	SELECT	rh.destination_database_name,
 			bs.backup_finish_date, 
 			rh.restore_date,
@@ -7,13 +12,20 @@
 	JOIN msdb.dbo.backupset bs on rh.backup_set_id = bs.backup_set_id
 	WHERE rh.restore_date > DATEADD(d,-14,GETUTCDATE())
 	AND bs.type IN('D','I','L','P','Q')
-
+	AND rh.destination_database_name IS NOT NULL
 )
+INSERT INTO #LastRestore WITH(TABLOCK) (destination_database_name,backup_finish_date,restore_date)
+SELECT	LR.destination_database_name,
+		LR.backup_finish_date,
+		LR.restore_date
+FROM LR
+WHERE LR.rnum=1;
+
 SELECT	d.name,
 		DATEADD(mi,DATEDIFF(mi,GETDATE(),GETUTCDATE()),LR.backup_finish_date) AS backup_finish_date, 
 		DATEADD(mi,DATEDIFF(mi,GETDATE(),GETUTCDATE()),LR.restore_date) AS restore_date
 FROM sys.databases d 
-LEFT OUTER JOIN LR ON d.name =  LR.destination_database_name AND LR.rnum=1
+LEFT OUTER JOIN #LastRestore LR ON d.name =  LR.destination_database_name
 WHERE (d.state = 1 OR d.is_in_standby=1)
 AND d.recovery_model <> 3 /* Exclude Simple */
 /* Exclude databases with a restore in progress */
@@ -25,4 +37,4 @@ AND NOT EXISTS(	SELECT 1
 				AND L.resource_type='DATABASE'
 				AND L.resource_database_id = d.database_id
 				)
-ORDER BY LR.backup_finish_date
+ORDER BY LR.backup_finish_date;
